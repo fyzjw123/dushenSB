f32c86e0baa2ff6a8b31d9d054e9af6c23781fb5
dujiajun <dujiajun1994@163.com>
2016-11-12 01:02:05 +0800

    PlayQueue 1st edition

--- play_queue.py
+++ play_queue.py
@@ -1,31 +1,31 @@
 class PlayQueue:
 
-	head = None
-	insert_pos = None
+	def __init__(self):
+		self.head=None
+		self.insert_pos=None
 
-	@staticmethod
-	def insert_music(url, by_user):
+	def insert_music(self, url, by_user):
 		music = Node(url, by_user)
-		if head == None:
-			head = music
+		if self.head is None:
+			self.head = music
 			music.before = music
 			music.next = music
-			insert_pos = music
+			if music.by_user:
+				self.insert_pos = music
 		else:
 			if music.by_user:
-				__insert_music(music, insert_pos)
-				insert_pos = music
+				self.__insert_music(music, self.insert_pos)
+				self.insert_pos = music
 			else:
-				tail=head.before
-				__insert_music(music, tail)
+				tail=self.head.before
+				self.__insert_music(music, tail)
 				
-
-	@staticmethod
-	def __insert_music(music, pos):
-		if pos==None:
-			music.next=head
-			music.before=head.before
-			head=music
+	def __insert_music(self, music, pos):
+		if pos is None:
+			music.next=self.head
+			music.before=self.head.before
+			self.head=music
+			return
 		music.next=pos.next
 		music.before=pos
 		music.next.before=music
@@ -34,31 +34,49 @@ class PlayQueue:
 	'''
 	point to next music
 	remove the music if it is added by the user and can only play once
-	!!!update insert_pos
+	!!!update self.insert_pos
 	'''
-	@staticmethod
-	def get_music():
-		if head == None:	return None
-		url = head.url
-		if head.by_user:
-			__remove_music(head)
+	def get_music(self):
+		if self.head is None:	return None
+		url = self.head.url
+		if self.head.by_user:
+			if self.head==self.insert_pos:
+				self.insert_pos=None
+			self.__remove_music(self.head)
+		else:
+			self.head=self.head.next
 		return url
 
-	
-	@staticmethod
-	def __remove_music(music):
+	def __remove_music(self, music):
 		if music.before==music.next:
-			head = None
-			insert_pos = None
+			self.head = None
+			self.insert_pos = None
 		else:
+			if music==self.head:
+				self.head = self.head.next
 			music.before.next = music.next
 			music.next.before = music.before 
-			if music==head:
-				head = head.next
-
 
 class Node:
 
 	def __init__(self, url, by_user):
 		self.url = url
 		self.by_user = by_user
+		self.before = None
+		self.next = None
+
+if __name__ == "__main__":
+	que =PlayQueue()
+	que.insert_music("a",False)
+	que.insert_music("b",False)
+	que.insert_music("c",True)
+	print(que.get_music())
+	que.insert_music("d",False)
+	print(que.get_music())
+	que.insert_music("e",True)
+	print(que.get_music())
+	print(que.get_music())
+	print(que.get_music())
+	print(que.get_music())
+	print(que.get_music())
+	print(que.get_music())

ec6ca9397a457ad0c82669043d90d12c422e0e16
钟健伟 <jianwei.zhong@ele.me>
2016-12-10 14:10:05 +0800

    push for server to pull

--- website/raspberry/pycode/config.py
+++ website/raspberry/pycode/config.py
@@ -1,3 +1,5 @@
 config_json = {
-    "url_prefix": "www.baidu.com"
+    "sogou_link": "http://mp3.sogou.com/music.so?query=%s\
+    &class=1&pf=&w=02009900&st=&ac=1&sourcetype=sugg\
+    &_asf=mp3.sogou.com&_ast=1361525645"
 }

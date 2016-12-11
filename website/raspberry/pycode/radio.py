8be3a090b2e2d63002e6f39ba31d228ab5e1c1e0
dujiajun <dujiajun1994@163.com>
2016-12-11 03:05:57 -0800

    change music info to array

--- website/raspberry/pycode/radio.py
+++ website/raspberry/pycode/radio.py
@@ -10,17 +10,17 @@ class Radio:
 
 	def __init__(self):
 		self.play_queue=PlayQueue()
-		self.play_queue.insert_music("%s/黄祖波\ -\ 平凡之路\ -\ 铃声版.mp3" % MUSIC_DIR, "平凡之路", False);
-		self.play_queue.insert_music("%s/金南玲\ -\ 逆流成河\ -\ 铃声版.mp3" % MUSIC_DIR, "逆流成河", False);
-		self.play_queue.insert_music("%s/田馥甄\ -\ 小幸运\ -\ 铃声版.mp3" % MUSIC_DIR, "小幸运", False);
-		self.play_queue.insert_music("%s/庄心妍\ -\ 走着走着就散了\ -\ 铃声版.mp3" % MUSIC_DIR, "走着走着就散了", False);
+		self.add_music("%s/黄祖波\ -\ 平凡之路\ -\ 铃声版.mp3" % MUSIC_DIR, ["平凡之路", "黄祖波"], False)
+		self.add_music("%s/金南玲\ -\ 逆流成河\ -\ 铃声版.mp3" % MUSIC_DIR, ["逆流成河", "金南玲"], False)
+		self.add_music("%s/田馥甄\ -\ 小幸运\ -\ 铃声版.mp3" % MUSIC_DIR, ["小幸运", "田馥甄"], False)
+		self.add_music("%s/庄心妍\ -\ 走着走着就散了\ -\ 铃声版.mp3" % MUSIC_DIR, ["走着走着就散了", "庄心妍"], False)
 		threading.Thread(target = self.__start_play, name = "Radio.__start_play").start()
 
 	def __start_play(self):
 		while True:
-			music = self.play_queue.get_music()
-			print(music)
-			self.__play_music(music)
+			url = self.play_queue.get_music_url()
+			print(url)
+			self.__play_music(url)
 
 	def __play_music(self, url):
 		p1 = subprocess.Popen("sudo ffmpeg -i %s -f wav pipe:" % url,
@@ -35,16 +35,19 @@ class Radio:
 
 	'''
 	Input:
-		url, music's url
-		name, music's name
+		url, url of music
+		info, [name, title, ...]
 		by_user, true of false
 	'''
-	def add_music(self, url, name, by_user):
-		self.play_queue.insert_music(url, name, by_user)
+	def add_music(self, url, info, by_user):
+		self.play_queue.insert_music(url, info, by_user)
 
 	'''
 	Output:
-		array of [music_url, music_name]
+		array of [url, info, by user]
+		url is url of music
+		info is raw data like [name, title, url...]
+		by user is true or false
 	'''
 	def get_music_list(self):
 		return self.play_queue.get_music_list()
@@ -53,4 +56,3 @@ class Radio:
 if __name__ == "__main__":
 	radio = Radio()
 	print(radio.get_music_list())
-	radio.add_music("www.djfe.com/jd.mp3", "hello123", True)

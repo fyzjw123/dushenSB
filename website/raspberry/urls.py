722650f5b218e9e36c0e2a73d440bced315ad3a9
钟健伟 <jianwei.zhong@ele.me>
2016-12-12 09:29:18 +0800

    add main page route

--- website/raspberry/urls.py
+++ website/raspberry/urls.py
@@ -6,5 +6,6 @@ app_name = 'raspberry'
 urlpatterns = [
     url(r'^music$', views.music, name="play_music"),
     url(r'^search$', views.search_music, name="search_music"),
-    url(r'^music_list$', views.get_music_list, name="get_music_list")
+    url(r'^music_list$', views.get_music_list, name="get_music_list"),
+    url(r'^main.html$', views.get_main, name="get_main_page")
 ]
